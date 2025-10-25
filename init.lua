ab94d61194e450115c21af6f8f36eb86a8007af1
Joseph Hilland <josephhilland@gmail.com>
2025-10-24 22:08:30 -0600

    starting fresh instead, simple

--- init.lua
+++ init.lua
@@ -1,4 +1,28 @@
-require("sethy.core")
-require("sethy.lazy")
-require("current-theme")
-require("sethy.terminalpop")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+require("catppuccin").setup({flavour = "mocha"})
+vim.cmd.colorscheme "catppuccin-mocha"
